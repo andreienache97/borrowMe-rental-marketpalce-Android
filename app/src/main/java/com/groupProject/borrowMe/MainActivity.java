8a7259ae3fafb05277440d1b70477c9199af5462
dianacocos15 <dianacocos15@gmail.com>
2018-04-18 15:45:36 +0100

    test change

--- app/src/main/java/com/groupProject/borrowMe/MainActivity.java
+++ app/src/main/java/com/groupProject/borrowMe/MainActivity.java
@@ -158,7 +158,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.message_user) {
 
-        }
+        } //change
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
