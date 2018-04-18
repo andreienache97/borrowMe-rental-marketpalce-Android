cdd33e6091262fa91cd5bcdf05c563dfa5753022
calvin <myxstyle120@yahoo.com>
2018-04-18 16:24:47 +0100

    calvin

--- app/src/main/java/com/groupProject/borrowMe/MainActivity.java
+++ app/src/main/java/com/groupProject/borrowMe/MainActivity.java
@@ -158,7 +158,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.message_user) {
 
-        } //change2345
+        } //change
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
