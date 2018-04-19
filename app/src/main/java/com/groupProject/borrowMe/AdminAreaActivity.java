745c4568f6e1a80f3767e219ec015ef2f2be6566
Enache <97andreienache@gmail.com>
2018-04-19 13:33:05 +0100

    admin activity

--- app/src/main/java/com/groupProject/borrowMe/AdminAreaActivity.java
+++ app/src/main/java/com/groupProject/borrowMe/AdminAreaActivity.java
@@ -1,7 +1,11 @@
 package com.groupProject.borrowMe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Button;
 
 import com.groupProject.borrowMe.R;
 
@@ -11,5 +15,20 @@ public class AdminAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_area);
+
+        final AppCompatButton cCheck = (AppCompatButton) findViewById(R.id.check);
+
+
+        cCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent listItems = new Intent(AdminAreaActivity.this, CheckItems.class);
+                AdminAreaActivity.this.startActivity(listItems);
+            }
+
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            //      .setAction("Action", null).show();
+
+        });
     }
 }
