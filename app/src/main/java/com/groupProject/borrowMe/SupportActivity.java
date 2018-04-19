ca6d155ae66cc1e68f39e0493b1650389533f1ca
dianacocos15 <dianacocos15@gmail.com>
2018-04-19 23:49:45 +0100

    linked Java files & added intents (Calvin, please check the java files)

--- app/src/main/java/com/groupProject/borrowMe/SupportActivity.java
+++ app/src/main/java/com/groupProject/borrowMe/SupportActivity.java
@@ -1,12 +1,15 @@
 package com.groupProject.borrowMe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
+
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SupportActivity extends AppCompatActivity {
 
@@ -14,5 +17,28 @@ public class SupportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+
+        final Spinner spinner1 = findViewById(R.id.spinner1);
+        final EditText editText4 = findViewById(R.id.editText4);
+        final Button button = findViewById(R.id.button);
+
+        // Response received from the server
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+
+                    if (success) {
+                        String email = jsonResponse.getString("email");
+                        String name = jsonResponse.getString("name");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 }
