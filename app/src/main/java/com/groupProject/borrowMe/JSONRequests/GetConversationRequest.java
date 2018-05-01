d58bd62469db35605b85b901cb86678d7ccdb4d8
Enache <97andreienache@gmail.com>
2018-05-02 00:31:11 +0100

    chat

--- app/src/main/java/com/groupProject/borrowMe/JSONRequests/GetConversationRequest.java
+++ app/src/main/java/com/groupProject/borrowMe/JSONRequests/GetConversationRequest.java
@@ -7,7 +7,7 @@ import com.android.volley.toolbox.StringRequest;
 
 public class GetConversationRequest extends StringRequest {
 
-    private static final String GET_MESSAGES_REQUEST_URL = "https://myxstyle120.000webhostapp.com/get_conversation.php";
+    private static final String GET_MESSAGES_REQUEST_URL = "https://myxstyle120.000webhostapp.com/get_conversation.php?email1=%1$s&email2=%2$s&item_id=%3$s";
 
     public GetConversationRequest(String email1, String email2, String itemId, Response.Listener<String> listener) {
         super(Request.Method.GET, String.format(GET_MESSAGES_REQUEST_URL, email1, email2, itemId), listener, null);
--- app/src/main/java/com/groupProject/borrowMe/JSONRequests/GetMessagesRequest.java
+++ app/src/main/java/com/groupProject/borrowMe/JSONRequests/GetMessagesRequest.java
@@ -8,7 +8,7 @@ import com.android.volley.toolbox.StringRequest;
 
 public class GetMessagesRequest extends StringRequest {
 
-    private static final String GET_MESSAGES_REQUEST_URL = "https://myxstyle120.000webhostapp.com/get_messages.php";
+    private static final String GET_MESSAGES_REQUEST_URL = "https://myxstyle120.000webhostapp.com/get_messages.php?email=%1$s";
 
     public GetMessagesRequest(String email, Response.Listener<String> listener) {
         super(Request.Method.GET, String.format(GET_MESSAGES_REQUEST_URL, email), listener, null);
