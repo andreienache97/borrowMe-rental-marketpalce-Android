2e091d0339de5acf25a1b04f5016c57d5b47c9fd
Caz <s.arocha@hotmail.com>
2018-05-03 02:55:15 +0100

    Search Bar works

--- app/src/main/java/com/groupProject/borrowMe/Departments/ItemSearch.java
+++ app/src/main/java/com/groupProject/borrowMe/Departments/ItemSearch.java
@@ -54,7 +54,7 @@ public class ItemSearch extends AppCompatActivity{
         //return button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+//test
 
         recyclerView = (RecyclerView) findViewById(R.id.ItemsAdapter);
         recyclerView.setHasFixedSize(true);
