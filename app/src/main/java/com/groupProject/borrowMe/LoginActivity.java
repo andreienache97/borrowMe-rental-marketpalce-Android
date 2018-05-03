6a8796d259a4aaac22331e4a067d2cabc3be0ec9
calvin <myxstyle120@yahoo.com>
2018-05-03 09:33:11 +0100

    Calvin

--- app/src/main/java/com/groupProject/borrowMe/LoginActivity.java
+++ app/src/main/java/com/groupProject/borrowMe/LoginActivity.java
@@ -9,6 +9,7 @@ package com.groupProject.borrowMe;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +26,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
 
 //Set the Edit text, TextView, Button
         final EditText etEmail = (EditText) findViewById(R.id.etEmail);
