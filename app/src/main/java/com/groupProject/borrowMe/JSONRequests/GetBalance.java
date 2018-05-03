85b712f9cbaf13bd2bdfaf808d96d3b039b36a8c
Caz <s.arocha@hotmail.com>
2018-05-03 14:22:37 +0100

    It Kinda might work

--- app/src/main/java/com/groupProject/borrowMe/JSONRequests/GetBalance.java
+++ app/src/main/java/com/groupProject/borrowMe/JSONRequests/GetBalance.java
@@ -10,6 +10,7 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
+//Balance
 public class GetBalance extends StringRequest {
     private static final String REGISTER_REQUEST_URL = "https://myxstyle120.000webhostapp.com/GetBalance.php";
     private Map<String, String> params;
